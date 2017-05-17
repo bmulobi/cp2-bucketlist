301a05cc8a30e717b28269b9c560dfe2e3b0087f
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-05-17 13:41:19 +0300

    fix pep8 issue

--- manage.py
+++ manage.py
@@ -5,11 +5,11 @@ from flask_script import Manager  # class for handling a set of commands
 
 from app import db, create_app
 
-app = create_app(config_name=os.getenv('APP_SETTINGS'))
+app = create_app(config_name=os.getenv("APP_SETTINGS"))
 migrate = Migrate(app, db)
 manager = Manager(app)
 
-manager.add_command('db', MigrateCommand)
+manager.add_command("db", MigrateCommand)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager.run()
