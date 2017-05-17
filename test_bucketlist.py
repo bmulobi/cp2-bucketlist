97ffa6ee606b63fbed8ee1e01978c408f2050e21
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-05-18 00:12:56 +0300

    add tests

--- test_bucketlist.py
+++ test_bucketlist.py
@@ -1,7 +1,8 @@
 import json
-import unittest
+import os
+import unittest, time
 
-from app import create_app, db
+from app import create_app, db, models
 
 
 class BucketlistTestCase(unittest.TestCase):
@@ -17,6 +18,29 @@ class BucketlistTestCase(unittest.TestCase):
             # create all tables
             db.create_all()
 
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
+
+    def test_app_settings_configuration(self):
+        """Test app settings"""
+        app_settings = os.getenv("APP_SETTINGS")
+        self.assertEqual(app_settings, "development")
+
+    def test_prodcution_database_path(self):
+        """Test path to production database"""
+        database = os.getenv("DATABASE_URL")
+        self.assertEqual(database, "postgresql://localhost/buckets")
+
+    def test_app_entry_settings(self):
+        """Test app entry settings"""
+
+        executable = os.getenv("FLASK_APP")
+        self.assertEqual(executable, "run.py")
+
     def test_api_can_register_new_user(self):
         """Test API can register new user (POST - "/bucketlists/v1.0/auth/register/')"""
 
@@ -24,13 +48,60 @@ class BucketlistTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 201)
         self.assertIn("ben", str(res.data))
 
+    def test_api_rejects_invalid_user_names(self):
+        """Test api checks usernames for invalid formats"""
+
+        res = self.client().post("/bucketlists/v1.0/auth/register/",
+                                 data={"username": "bendbvg987^&%^$#$__8765634&*^^%$#$#@$%",
+                                       "password": 1234}
+                                 )
+        self.assertEqual(res.status_code, 400)
+        self.assertIn('"message": "Invalid username format"', str(res.data))
+
+    def test_api_rejects_user_names_longer_than_50_characters(self):
+        """Test api checks usernames for excess length"""
+
+        res = self.client().post("/bucketlists/v1.0/auth/register/",
+                           data={"username": "greater-than-fifty-greater-than-fifty-"+
+                                             "greater-than-fifty-greater-than-fifty",
+                                 "password": 1234})
+
+        self.assertEqual(res.status_code, 400)
+        self.assertIn('"message": "Name should be <= 50 charac"', str(res.data))
+
+
     def test_api_can_login_valid_user(self):
         """Test API can login valid user (POST - '/bucketlists/v1.0/auth/login/')"""
 
-        res = self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
+        self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
         res = self.client().post("/bucketlists/v1.0/auth/login/", data={"username_or_token": "ben", "password": 1234})
         self.assertEqual(res.status_code, 200)
         self.assertIn('"username": "ben"', str(res.data))
+
+    def test_api_rejects_invalid_token(self):
+        """Test API rejects invalid token"""
+        self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
+        res = self.client().post("/bucketlists/v1.0/auth/login/", data={"username_or_token": "ben", "password": 1234})
+        result_in_json = json.loads(res.data.decode("utf-8"))
+        token = result_in_json['token']
+
+        corrupted_token = token + "additional-characters"
+        res2 = self.client().post("/bucketlists/v1.0/", data={"name": "make billions"}, headers={"token": corrupted_token})
+        self.assertEqual(res2.status_code, 403)
+        self.assertIn('"message": "Invalid token"', str(res2.data))
+
+    def test_api_rejects_expired_token(self):
+        """Test API rejects expired token"""
+        self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
+        self.client().post("/bucketlists/v1.0/auth/login/", data={"username_or_token": "ben", "password": 1234})
+
+        user = models.Users("ben","1234")
+        new_token = user.generate_auth_token(2)
+        time.sleep(5)
+        res2 = self.client().post("/bucketlists/v1.0/", data={"name": "make billions"},
+                                  headers={"token": new_token})
+        self.assertEqual(res2.status_code, 403)
+        self.assertIn('"message": "Expired token, "', str(res2.data))
 
     def test_api_can_create_new_bucketlist(self):
         """Test API can create bucketlist (POST - '/bucketlists/v1.0/')"""
@@ -43,6 +114,36 @@ class BucketlistTestCase(unittest.TestCase):
         res2 = self.client().post("/bucketlists/v1.0/", data={"name":"make billions"}, headers={"token":token})
         self.assertEqual(res2.status_code, 201)
         self.assertIn('"name": "make billions"', str(res2.data))
+
+    def test_api_rejects_invalid_bucket_names(self):
+        """Test api checks bucket names for invalid formats"""
+
+        self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
+        res1 = self.client().post("/bucketlists/v1.0/auth/login/", data={"username_or_token": "ben", "password": 1234})
+        result_in_json = json.loads(res1.data.decode("utf-8"))
+        token = result_in_json['token']
+        res2 = self.client().post("/bucketlists/v1.0/",
+                                  data={"name": ["(**^%$$#@!#$&*()&*^%$%$#@*&*&%$%@#"]},
+                                  headers={"token": token}
+                                  )
+        self.assertEqual(res2.status_code, 400)
+        self.assertIn('"message": "Invalid bucketlist name format"', str(res2.data))
+
+    def test_api_rejects_bucket_names_longer_than_50_characters(self):
+        """Test api checks bucket names for excess length"""
+
+        self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
+        res1 = self.client().post("/bucketlists/v1.0/auth/login/",
+                                  data={"username_or_token": "ben", "password": 1234})
+        result_in_json = json.loads(res1.data.decode("utf-8"))
+        token = result_in_json['token']
+        res2 = self.client().post("/bucketlists/v1.0/",
+                                  data={"name": "greater-than-fifty-greater-than-fifty-"+
+                                  "greater-than-fifty-greater-than-fifty"},
+                                  headers={"token": token}
+                                  )
+        self.assertEqual(res2.status_code, 400)
+        self.assertIn('"message": "Name should be <= 50 charac"', str(res2.data))
 
     def test_bucketlist_creation_requires_name_parameter(self):
         """Test API requires bucket name in order to create it"""
@@ -79,7 +180,7 @@ class BucketlistTestCase(unittest.TestCase):
         token = result_in_json['token']
 
         self.client().post("/bucketlists/v1.0/", data={"name": "live to be 100"}, headers={"token": token})
-        result = self.client().get("/bucketlists/v1.0/1")
+        result = self.client().get("/bucketlists/v1.0/1", headers={"token": token})
 
         self.assertEqual(result.status_code, 200)
         self.assertIn("live to be 100", str(result.data))
@@ -99,53 +200,77 @@ class BucketlistTestCase(unittest.TestCase):
     def test_bucketlist_can_be_edited(self):
         """Test API can edit an existing bucketlist. (PUT request)"""
         self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
-        res = self.client().post("/bucketlists/v1.0/", data={"name": "make billions"})
-        self.assertEqual(res.status_code, 201)
-        res = self.client().put(
-            "/bucketlists/1",
-            data={
-                "name": "Dont just eat, but also pray and love :-)"
-            })
-        self.assertEqual(res.status_code, 200)
-        results = self.client().get("/bucketlists/1")
-        self.assertIn("Dont just eat", str(results.data))
-
-    #################################################################################################################
-    # POST /auth/login                            Logs a user in
-
-    # POST /auth/register                         Register a user
-
-    # POST /bucketlists/                          Create a new bucket list
-    # GET /bucketlists/                           List all the created bucket lists
-
-    # GET /bucketlists/<id>                       Get single bucket list
-    # PUT /bucketlists/<id>                       Update this bucket list
-    # DELETE /bucketlists/<id>                    Delete this single bucket list
-
-    # POST /bucketlists/<id>/items/               Create a new item in bucket list
-
-    # PUT /bucketlists/<id>/items/<item_id>       Update a bucket list item
-    # DELETE /bucketlists/<id>/items/<item_id>    Delete an item in a bucket list
-    #################################################################################################################
+        res1 = self.client().post("/bucketlists/v1.0/auth/login/", data={"username_or_token": "ben", "password": 1234})
+        result_in_json = json.loads(res1.data.decode("utf-8"))
+        token = result_in_json['token']
+        self.client().post("/bucketlists/v1.0/", data={"name": "make billions"}, headers={"token": token})
+        res2 = self.client().put("/bucketlists/v1.0/1",data={"name": "make trillions"}, headers={"token": token})
+        self.assertEqual(res2.status_code, 200)
+        results = self.client().get("/bucketlists/v1.0/1", headers={"token": token})
+        self.assertIn('"name": "make trillions"', str(results.data))
 
     def test_bucketlist_deletion(self):
         """Test API can delete an existing bucketlist. (DELETE request)."""
-        rv = self.client().post(
-            "/bucketlists/",
-            data={"name": "Eat, pray and love"})
-        self.assertEqual(rv.status_code, 201)
-        res = self.client().delete("/bucketlists/1")
-        self.assertEqual(res.status_code, 200)
-        # Test to see if it exists, should return a 404
-        result = self.client().get("/bucketlists/1")
-        self.assertEqual(result.status_code, 404)
+        self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
+        res1 = self.client().post("/bucketlists/v1.0/auth/login/", data={"username_or_token": "ben", "password": 1234})
+        result_in_json = json.loads(res1.data.decode("utf-8"))
+        token = result_in_json['token']
+        self.client().post("/bucketlists/v1.0/", data={"name": "make billions"}, headers={"token": token})
+        self.client().delete("/bucketlists/v1.0/1", headers={"token": token})
+        res2 = self.client().get("/bucketlists/v1.0/1", headers={"token": token})
 
-    def tearDown(self):
-        """teardown all initialized variables."""
-        with self.app.app_context():
-            # drop all tables
-            db.session.remove()
-            db.drop_all()
+        self.assertEqual(res2.status_code, 404)
+
+    def test_can_create_new_item_in_bucketlist(self):
+        """Test API can create new item in bucketlist. (POST request)."""
+        self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
+        res1 = self.client().post("/bucketlists/v1.0/auth/login/", data={"username_or_token": "ben", "password": 1234})
+        result_in_json = json.loads(res1.data.decode("utf-8"))
+        token = result_in_json['token']
+        self.client().post("/bucketlists/v1.0/", data={"name": "make billions"}, headers={"token": token})
+        res2 = self.client().post("/bucketlists/v1.0/1/items/", data={"name": "make app"}, headers={"token": token})
+
+        self.assertEqual(res2.status_code, 201)
+
+    def test_api_rejects_invalid_item_names(self):
+        """Test API can create new item in bucketlist. (POST request)."""
+        self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
+        res1 = self.client().post("/bucketlists/v1.0/auth/login/",
+                                  data={"username_or_token": "ben", "password": 1234})
+        result_in_json = json.loads(res1.data.decode("utf-8"))
+        token = result_in_json['token']
+        self.client().post("/bucketlists/v1.0/", data={"name": "make billions"}, headers={"token": token})
+        res2 = self.client().post("/bucketlists/v1.0/1/items/", data={"name": ("()**^%$%$#&^&%^%$$%$$^^^*")}, headers={"token": token})
+
+        self.assertEqual(res2.status_code, 400)
+        self.assertIn('"message":"Invalid item name format"', str(res2.data))
+
+
+    def test_can_update_an_item_in_bucketlist(self):
+        """Test API can update an item in bucketlist. (POST request)."""
+        self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
+        res1 = self.client().post("/bucketlists/v1.0/auth/login/", data={"username_or_token": "ben", "password": 1234})
+        result_in_json = json.loads(res1.data.decode("utf-8"))
+        token = result_in_json['token']
+        self.client().post("/bucketlists/v1.0/", data={"name": "make billions"}, headers={"token": token})
+        self.client().post("/bucketlists/v1.0/1/items/", data={"name": "make app"}, headers={"token": token})
+        res2 = self.client().put("/bucketlists/v1.0/1/items/1", data={"name": "updated app"}, headers={"token": token})
+
+        self.assertEqual(res2.status_code, 201)
+
+    def test_can_delete_an_item_in_bucketlist(self):
+        """Test API can delete an item in bucketlist. (DELETE request)."""
+        self.client().post("/bucketlists/v1.0/auth/register/", data={"username": "ben", "password": 1234})
+        res1 = self.client().post("/bucketlists/v1.0/auth/login/", data={"username_or_token": "ben", "password": 1234})
+        result_in_json = json.loads(res1.data.decode("utf-8"))
+        token = result_in_json['token']
+        self.client().post("/bucketlists/v1.0/", data={"name": "make billions"}, headers={"token": token})
+        res2 = self.client().post("/bucketlists/v1.0/1/items/", data={"name": "make app"}, headers={"token": token})
+        self.assertEqual(res2.status_code, 201)
+        self.client().delete("/bucketlists/v1.0/1/items/1", data={"name": "updated app"},
+                                    headers={"token": token})
+        res3 = self.client().get("/bucketlists/v1.0/1", headers={"token": token})
+        self.assertNotIn('"name": "make app"', str(res3.data))
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
