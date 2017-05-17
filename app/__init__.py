90f169081004fa2a3d1f0e24f0fbad016ba88ba7
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-05-17 13:37:44 +0300

    added flask-REStful urls

--- app/__init__.py
+++ app/__init__.py
@@ -16,48 +16,43 @@ def create_app(config_name):
                                UserLoginAPI,
                                BucketListAPI,
                                BucketListItemsAPI,
-                               BucketListItemAPI
+                               BucketListItemAPI,
+                               GetTokenAPI
                               )
 
 
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_pyfile("config.py")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
     api = Api(app)
 
     # routes
-    api.add_resource(BucketListsAPI, '/bucketlists/v1.0/', endpoint = 'bucketlists')
-    api.add_resource(UserRegistrationAPI, '/bucketlists/v1.0/auth/register/', endpoint='registration')
-    api.add_resource(UserLoginAPI, '/bucketlists/v1.0/auth/login/', endpoint='login')
-    api.add_resource(BucketListAPI, '/bucketlists/v1.0/<id>/', endpoint='bucketlist')
-    api.add_resource(BucketListItemsAPI, '/bucketlists/v1.0/<id>/items/', endpoint='items')
-    api.add_resource(BucketListItemAPI, '/bucketlists/v1.0/<id>/items/<item_id>', endpoint='item')
+    api.add_resource(UserRegistrationAPI, "/bucketlists/v1.0/auth/register/", endpoint="registration")
+    api.add_resource(UserLoginAPI, "/bucketlists/v1.0/auth/login/", endpoint="login")
+    api.add_resource(GetTokenAPI, "/bucketlists/v1.0/token/", endpoint="token")
+    api.add_resource(BucketListsAPI, "/bucketlists/v1.0/", endpoint = "bucketlists")
+    api.add_resource(BucketListAPI, "/bucketlists/v1.0/<id>/", endpoint="bucketlist")
+    api.add_resource(BucketListItemsAPI, "/bucketlists/v1.0/<id>/items/", endpoint="items")
+    api.add_resource(BucketListItemAPI, "/bucketlists/v1.0/<id>/items/<item_id>", endpoint="item")
 
 
-
-
-
-
-
-
-
-    # @app.route('/bucketlists/', methods=['POST', 'GET'])
+    # @app.route('/bucketlists/", methods=["POST", "GET"])
     # def bucketlists():
     #     if request.method == "POST":
     #
-    #         name = str(request.data.get('name', ''))
+    #         name = str(request.data.get("name", ""))
     #
     #         if name:
     #             bucketlist = Bucketlist(name=name)
     #             bucketlist.save()
     #             response = jsonify({
-    #                 'id': bucketlist.id,
-    #                 'name': bucketlist.name,
-    #                 'date_created': bucketlist.date_created,
-    #                 'date_modified': bucketlist.date_modified
+    #                 "id": bucketlist.id,
+    #                 "name": bucketlist.name,
+    #                 "date_created": bucketlist.date_created,
+    #                 "date_modified": bucketlist.date_modified
     #             })
     #             response.status_code = 201
     #
@@ -74,49 +69,49 @@ def create_app(config_name):
     #
     #         for bucketlist in bucketlists:
     #             obj = {
-    #                 'id': bucketlist.id,
-    #                 'name': bucketlist.name,
-    #                 'date_created': bucketlist.date_created,
-    #                 'date_modified': bucketlist.date_modified
+    #                 "id": bucketlist.id,
+    #                 "name": bucketlist.name,
+    #                 "date_created": bucketlist.date_created,
+    #                 "date_modified": bucketlist.date_modified
     #             }
     #             results.append(obj)
     #         response = jsonify(results)
     #         response.status_code = 200
     #         return response
     #
-    # @app.route('/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    # @app.route("/bucketlists/<int:id>", methods=["GET", "PUT", "DELETE"])
     # def bucketlist_manipulation(id, **kwargs):
-    #     # retrieve a buckelist using it's ID
+    #     # retrieve a buckelist using it"s ID
     #     bucketlist = Bucketlist.query.filter_by(id=id).first()
     #     if not bucketlist:
     #         # Raise an HTTPException with a 404 not found status code
     #         abort(404)
     #
-    #     if request.method == 'DELETE':
+    #     if request.method == "DELETE":
     #         bucketlist.delete()
     #         return {
     #                    "message": "bucketlist {} deleted successfully".format(bucketlist.id)
     #                }, 200
     #
-    #     elif request.method == 'PUT':
-    #         name = str(request.data.get('name', ''))
+    #     elif request.method == "PUT":
+    #         name = str(request.data.get("name", ""))
     #         bucketlist.name = name
     #         bucketlist.save()
     #         response = jsonify({
-    #             'id': bucketlist.id,
-    #             'name': bucketlist.name,
-    #             'date_created': bucketlist.date_created,
-    #             'date_modified': bucketlist.date_modified
+    #             "id": bucketlist.id,
+    #             "name": bucketlist.name,
+    #             "date_created": bucketlist.date_created,
+    #             "date_modified": bucketlist.date_modified
     #         })
     #         response.status_code = 200
     #         return response
     #     else:
     #         # GET
     #         response = jsonify({
-    #             'id': bucketlist.id,
-    #             'name': bucketlist.name,
-    #             'date_created': bucketlist.date_created,
-    #             'date_modified': bucketlist.date_modified
+    #             "id": bucketlist.id,
+    #             "name": bucketlist.name,
+    #             "date_created": bucketlist.date_created,
+    #             "date_modified": bucketlist.date_modified
     #         })
     #         response.status_code = 200
     #         return response
