fd175c050337b4631bda4a74245acba503511b02
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-05-22 15:32:04 +0300

    [Feature] change settings path

--- manage.py
+++ manage.py
@@ -4,9 +4,8 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager  # class for handling a set of commands
 
 from app import db, create_app
-from instance import config
 
-app = create_app(config_name="development")
+app = create_app(config_name=os.getenv("APP_SETTINGS"))
 migrate = Migrate(app, db)
 manager = Manager(app)
 
