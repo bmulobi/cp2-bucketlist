c0726a682308c8dfeb1b0f090d0908cd0c512cf4
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-05-22 11:39:10 +0300

    [feature] add pagination parameters

--- instance/config.py
+++ instance/config.py
@@ -6,10 +6,18 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET = os.getenv("SECRET")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    BUCKETLISTS_PER_PAGE = 20
+    MIN_BUCKETLISTS_PER_REQUEST = 1
+    MAX_BUCKETLISTS_PER_REQUEST = 100
+    JSON_SORT_KEYS = False
+
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
+    BUCKETLISTS_PER_PAGE = 2
+    MAX_BUCKETLISTS_PER_REQUEST = 10
+
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
