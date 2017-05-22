154c70053e1d8e3876cdc004ef4e494dda90355f
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-05-22 11:37:09 +0300

    [feature] add parameter data types

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,6 @@ def create_app(config_name):
                                UserLogOutAPI
                               )
 
-
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile("config.py")
@@ -35,9 +34,9 @@ def create_app(config_name):
     api.add_resource(UserLoginAPI, "/bucketlists/v1.0/auth/login/", endpoint="login")
     api.add_resource(GetTokenAPI, "/bucketlists/v1.0/token/", endpoint="token")
     api.add_resource(BucketListsAPI, "/bucketlists/v1.0/", endpoint = "bucketlists")
-    api.add_resource(BucketListAPI, "/bucketlists/v1.0/<id>", endpoint="bucketlist")
-    api.add_resource(BucketListItemsAPI, "/bucketlists/v1.0/<id>/items/", endpoint="items")
-    api.add_resource(BucketListItemAPI, "/bucketlists/v1.0/<id>/items/<item_id>", endpoint="item")
+    api.add_resource(BucketListAPI, "/bucketlists/v1.0/<int:id>", endpoint="bucketlist")
+    api.add_resource(BucketListItemsAPI, "/bucketlists/v1.0/<int:id>/items/", endpoint="items")
+    api.add_resource(BucketListItemAPI, "/bucketlists/v1.0/<int:id>/items/<int:item_id>", endpoint="item")
     api.add_resource(UserLogOutAPI, "/bucketlists/v1.0/auth/logout/", endpoint="logout")
 
     return app
