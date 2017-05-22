74db17a98d7e9de41a8c58b64fe0bec4265540b2
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-05-22 12:48:25 +0300

    [Bug] fix environment error

--- manage.py
+++ manage.py
@@ -4,8 +4,9 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager  # class for handling a set of commands
 
 from app import db, create_app
+from instance import config
 
-app = create_app(config_name=os.getenv("APP_SETTINGS"))
+app = create_app(config_name=config.app_config["development"])
 migrate = Migrate(app, db)
 manager = Manager(app)
 
