fcbe304809eab51857c9d2687605f86739abacd5
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-05-23 21:49:58 +0300

    [Features] add commands

--- manage.py
+++ manage.py
@@ -5,11 +5,25 @@ from flask_script import Manager  # class for handling a set of commands
 
 from app import db, create_app
 
-app = create_app()
+app = create_app(config_name=os.getenv("APP_SETTINGS"))
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command("db", MigrateCommand)
+
+@manager.command
+def create_db(dbname):
+    """Creates database with tables"""
+
+    os.system('createdb ' + dbname)
+    db.create_all()
+    db.session.commit()
+
+
+@manager.command
+def drop_db(dbname):
+    """Deletes database"""
+    os.system('dropdb ' + dbname)
 
 if __name__ == "__main__":
     manager.run()
