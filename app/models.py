c9bfaaeac80ddf73c781ee81966c75e6f49077d2
Bernard Silla Mulobi <bernard.mulobi@andela.com>
2017-05-23 21:46:15 +0300

    [Feature] add doc strings

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ from app import db
 
 auth = HTTPBasicAuth()
 
+
 class Users(db.Model):
     """Class represents the users table"""
 
@@ -25,18 +26,21 @@ class Users(db.Model):
         self.user_password = pwd_context.encrypt(password)
 
     def hash_password(self, password):
+        """hash user password"""
         self.password_hash = pwd_context.encrypt(password)
 
     def verify_password(self, password):
+        """verify user password"""
         return pwd_context.verify(password, self.user_password)
 
-
     def generate_auth_token(self, expiration=600):
+        """generates authorization token"""
         s = Serializer(os.getenv("SECRET"), expires_in=expiration)
         return s.dumps({"id": self.id})
 
     @staticmethod
     def verify_auth_token(token):
+        """verifies authorization token"""
         s = Serializer(os.getenv("SECRET"))
         try:
             data = s.loads(token)
@@ -48,22 +52,27 @@ class Users(db.Model):
         return user
 
     def save(self):
+        """saves new user object"""
         db.session.add(self)
         db.session.commit()
 
     @staticmethod
     def get_all():
+        """gets all user objects"""
         return Users.query.all()
 
     @staticmethod
     def get_one(user_id):
+        """get's single user object given id"""
         return Users.query.filter_by(id=user_id).first()
 
     def delete(self):
+        """deletes current user object"""
         db.session.delete(self)
         db.session.commit()
 
     def __repr__(self):
+        """user object string representation"""
         return "<Users: {}>".format(self.user_name)
 
 
@@ -83,26 +92,31 @@ class Bucketlists(db.Model):
                             cascade="save-update, merge, delete", lazy="dynamic")
 
     def __init__(self, name, user_id):
-        """initialize with name."""
+        """initialize with name and user id"""
         self.name = name
         self.created_by = user_id
 
     def save(self):
+        """save new bucketlist object"""
         db.session.add(self)
         db.session.commit()
 
     @staticmethod
     def get_all_for_user(user_id=0):
+        """get all bucketlists for user with given id"""
         return Bucketlists.query.filter_by(created_by=user_id)
 
     def delete(self):
+        """delete current object"""
         db.session.delete(self)
         db.session.commit()
 
     def update(self):
+        """update current object"""
         db.session.commit()
 
     def __repr__(self):
+        """bucketlist object string representation"""
         return "<Bucketlists: {}>".format(self.name)
 
 
@@ -121,24 +135,29 @@ class BucketListItems(db.Model):
     bucket_id = db.Column(db.Integer, db.ForeignKey("bucketlists.id"))
 
     def __init__(self, description, bucket_id):
-        """initialize with description."""
+        """initialize with description and bucketlist id"""
         self.description = description
         self.bucket_id = bucket_id
 
     def save(self):
+        """save new bucketlist item object"""
         db.session.add(self)
         db.session.commit()
 
     @staticmethod
     def get_all_for_bucket(bucket_id):
+        """get all items in bucket with given id"""
         return BucketListItems.query.filter(created_by=bucket_id)
 
     def delete(self):
+        """delete current object"""
         db.session.delete(self)
         db.session.commit()
 
     def update(self):
+        """update current object"""
         db.session.commit()
 
     def __repr__(self):
+        """string representation for object"""
         return "<Bucketlists: {}>".format(self.name)
